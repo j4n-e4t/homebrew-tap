ca0994b415f9cd1d94059b706e18408e810687c4
Julian <130256240+j4n-e4t@users.noreply.github.com>
2026-03-01 01:30:51 +0100

    Update paperless-cli checksum

--- Formula/paperless-cli.rb
+++ Formula/paperless-cli.rb
@@ -3,7 +3,7 @@ class PaperlessCli < Formula
   homepage "https://github.com/j4n-e4t/paperless-cli"
   version "0.1.0"
   url "https://github.com/j4n-e4t/paperless-cli/releases/download/v#{version}/paperless-cli-#{version}.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "3d0845dc742451658e34396807d684fd40bbba2256554a11125399cf088823ca"
   license "REPLACE_WITH_PROJECT_LICENSE"
 
   depends_on "node"
